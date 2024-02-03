bd547911f1896fb808bb342c588d3103c7dee359
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-03 16:38:31 +0300

    arithmetic operator

---
+++ Arithmetic-operators/index.js
@@ -0,0 +1,34 @@
+// arithmetic operators = operands (value, variable , etc.)
+// operators (+ - * /)
+// export. 11 = x + 5;
+
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+// students = students % 2;
+
+// students += 2;
+// students -= 2;
+// students *= 2;
+// students /= 2;
+// students **= 2;
+// students %= 2;
+
+// students++;
+// students--;
+
+/*
+operators predence
+
+1. parenthesis ()
+2. exponents **
+3. multiplication & division & modulo
+4. addition & subtraction
+*/
+
+let result = 1 + 2 * 3 + 4 ** 2;
+console.log(result);
