0028019293fbc58812ba1dcb0831e076f6ecaed2
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-03 22:13:40 +0300

    math object

---
+++ 8_Math-object/index.js
@@ -0,0 +1,29 @@
+// Math = built-in object that provides a
+//        collection of properties and methods
+
+// console.log(Math.PI);
+// console.log(Math.E);
+
+let x = 3.21;
+let k = 3.33;
+let y = 2;
+let a = 2;
+let b = 3;
+let z;
+
+// z = Math.round(x); number rounding
+// z = Math.floor(k);  rounding down
+// z = Math.ceil(k); rounding up
+// z = Math.trunc(k);  deletes the decimal part
+// z = Math.pow(a, b);  exponentiation
+// z = Math.sqrt(x); taking root
+// z = Math.log(x);
+// z = Math.sin(x);
+// z = Math.cos(x);
+// z = Math.tan(x);
+// z = Math.abs(x);
+// z = Math.sign(x);
+// let max = Math.max(x, y, z);
+// let min = Math.min(x, y, z);
+
+console.log(z);
