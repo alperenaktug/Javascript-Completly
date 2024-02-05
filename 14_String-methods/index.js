f3716ae45ff1f269b76ca630929cec17f431aaa8
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-05 15:21:29 +0300

    string methods

---
+++ 14_String-methods/index.js
@@ -0,0 +1,39 @@
+// string methods = allow you to manipulate with text (strings)
+
+// .charAt()  Verilen dizindeki illk harfi verir.
+// .indexOf() Kaçıncı indekste olduğunun sayısını verir.
+// .lastIndexOf() sondan kaçıncı sırada olduğu
+// .length toplam kaç akarakterden oluşturğu
+// .trim() kırpma işleminde
+// .toUpperCase() büyük harfe çevirmek
+// .toLowerCase() küçük harfe çevirmek
+// .repeat()
+// .startWith()
+// .endsWith()
+// .includes()
+// .replaceAll()
+// .padStart()
+// .padEnd()
+// conclusion
+
+let userName = "JackNicholson";
+
+console.log(userName.charAt(2));
+
+let userName1 = "JackNicholson";
+
+console.log(userName1.indexOf("a"));
+
+let userName2 = "JackNicholson";
+
+console.log(userName2.lastIndexOf("n"));
+
+let userName3 = "JackNicholson";
+
+console.log(userName3.length);
+
+let userName4 = "JackNicholson";
+
+console.log(userName4.toUpperCase());
+console.log(userName4.toLowerCase());
+console.log(userName4.repeat(3));
