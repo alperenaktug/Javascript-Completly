f48c7372475bd42873c2cb1093d948430ba57a2a
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-05 16:06:52 +0300

    logical operator

---
+++ 17_Logical-operators/index.js
@@ -0,0 +1,28 @@
+// logical operators = used to combine or manipulate boolean values
+//                   (true , false)
+
+//                    AND = &&
+//                    OR = ||
+//                    NOT = !
+
+// const temp = -100;
+
+// if (temp > 0 && temp < 30) {
+//   console.log("The weather is GOOD!!");
+// } else {
+//   console.log("The weather is BAD !!");
+// }
+
+// if (temp < 0 || temp > 30) {
+//   console.log("The weather is BAD!!");
+// } else {
+//   console.log("The weather is GOOD !!");
+// }
+
+const isSunny = true;
+
+if (!isSunny) {
+  console.log("It is CLOUDLY");
+} else {
+  console.log("It is SUNNY");
+}
