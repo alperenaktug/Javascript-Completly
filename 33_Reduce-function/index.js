01479a22e81ad6448a6923146a31a8aa288251d5
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-09 12:49:44 +0300

    reduce

---
+++ 33_Reduce-function/index.js
@@ -0,0 +1,25 @@
+// .reduce() = reduce the elements of an array to a single value
+// bir dizinin elemanlarını tek bir değere indirger
+
+// const prices = [5, 30, 10, 25, 15, 20];
+
+// const total = prices.reduce(sum);
+// console.log(`$${total.toFixed(2)}`);
+
+// function sum(accumulator, element) {
+//   return accumulator + element;
+// }
+
+const grades = [75, 50, 90, 80, 65, 95];
+
+const maximum = grades.reduce(getMin);
+
+console.log(maximum);
+
+function getMax(accumulator, element) {
+  return Math.max(accumulator, element);
+}
+
+function getMin(accumulator, element) {
+  return Math.min(accumulator, element);
+}
