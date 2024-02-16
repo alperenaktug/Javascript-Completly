8eb30b8909b3014b575f5fe20ee26a982f93319b
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-02-16 16:12:02 +0300

    fetch Api

--- 58_Async-Await/index.js
+++ 58_Async-Await/index.js
@@ -5,6 +5,48 @@
 // Async doesn't have resolve or reject parameters
 // Everything after Await is placed in an event queue
 
+function walkDog() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const dogWalked = true;
+
+      if (dogWalked) {
+        resolve("You walk the dog 🐕");
+      } else {
+        reject("You DIDN'T walk the dog");
+      }
+    }, 1500);
+  });
+}
+
+function cleanKitchen() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const kitchenCleaned = true;
+
+      if (kitchenCleaned) {
+        resolve("You clean the kitchen 🧹");
+      } else {
+        reject("You DIDN'T clean the kitchen");
+      }
+    }, 2500);
+  });
+}
+
+function takeOutTrash() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const trashTakenOut = true;
+
+      if (trashTakenOut) {
+        resolve("You take out the trash ♻");
+      } else {
+        reject("You DIDN'T take out the trash");
+      }
+    }, 500);
+  });
+}
+
 async function doChores() {
   try {
     const walkDogResult = await walkDog();
